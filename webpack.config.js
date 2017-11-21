0f49da850011d2407bf48c93cdc4b18a41319d67
yangqi <yangqi@yangqideMacBook-Air.local>
2017-11-21 14:04:19 +0800

    update package.json

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   devtool: '#cheap-module-eval-source-map',
